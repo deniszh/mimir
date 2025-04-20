8efc18345c3175a4d1fe93e3f81e2e0f9bfb350e
Denys Zhdanov <denys.zhdanov@booking.com>
2025-04-20 19:05:20 +0200

    Adding MinLZ compression to ingesters

--- pkg/ingester/client/client.go
+++ pkg/ingester/client/client.go
@@ -18,6 +18,7 @@ import (
 	"github.com/grafana/mimir/pkg/mimirpb"
 	querierapi "github.com/grafana/mimir/pkg/querier/api"
 	"github.com/grafana/mimir/pkg/util"
+	"github.com/grafana/mimir/pkg/util/grpcencoding/minlz"
 	"github.com/grafana/mimir/pkg/util/grpcencoding/s2"
 	"github.com/grafana/mimir/pkg/util/grpcencoding/zstd"
 )
@@ -78,7 +79,7 @@ func (cfg *Config) RegisterFlags(f *flag.FlagSet) {
 }
 
 func (cfg *Config) RegisterFlagsWithPrefix(prefix string, f *flag.FlagSet) {
-	cfg.GRPCClientConfig.CustomCompressors = []string{s2.Name, zstd.Name}
+	cfg.GRPCClientConfig.CustomCompressors = []string{s2.Name, zstd.Name, minlz.Name}
 	cfg.GRPCClientConfig.RegisterFlagsWithPrefix(prefix, f)
 }
 
